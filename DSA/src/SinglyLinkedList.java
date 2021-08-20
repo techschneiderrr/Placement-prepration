7879a3ea91160be7a14e85566aa45b425a1aef03
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2021-08-20 14:09:27 +0530

    commit

---
+++ DSA/src/SinglyLinkedList.java
@@ -0,0 +1,35 @@
+
+public class SinglyLinkedList {
+
+	private ListNode head;
+	
+	private static class ListNode {
+		private int data;
+		private ListNode next;
+		
+		public ListNode(int data) {
+			this.data = data;
+			this.next = null;
+		}
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		 SinglyLinkedList sll = new SinglyLinkedList();
+		 sll.head = new ListNode(10);
+		 ListNode second = new ListNode(11);
+		 ListNode third = new ListNode(12);
+		 ListNode fourth = new ListNode(13);
+		 sll.head.next = second;
+		 second.next = third;
+		 third.next = fourth;
+		 ListNode current = sll.head;
+		 while(current!=null) {
+			System.out.print(current.data+" --> "); 
+			current = current.next;	 
+		 }
+		 System.out.println("Null");
+	}
+	
+}
