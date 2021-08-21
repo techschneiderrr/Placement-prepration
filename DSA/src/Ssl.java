fd5359cc54ca011c9c6810ba59c761b24cf72efd
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2021-08-21 15:10:36 +0530

    Update Ssl.java

--- DSA/src/Ssl.java
+++ DSA/src/Ssl.java
@@ -19,6 +19,7 @@ public class Ssl {
 		int value = sc.nextInt();
 		head = new Node(value);
 		display();
+		
 	}
 	
 	public void add() {
@@ -36,7 +37,54 @@ public class Ssl {
 			current = current.next;
 		}
 		display();
+		
 	}
+	
+	public void addstart() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the value to be added to start of linkedList : ");
+		int value = sc.nextInt();
+		Node node = new Node(value);
+		node.next = head;
+		head = node;
+		display();
+	}	
+	
+	
+	public void addmiddle() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the position in the linkedList : ");
+		int pos = sc.nextInt();
+		Node temp = head;
+		int tem = 0;
+		while(temp!=null) {
+			tem++;
+			temp=temp.next;
+		}
+		
+		if(pos>tem) {
+			System.out.println("Enter a valid number ! ");
+		}
+		else if(pos==1) {
+			addstart();
+		}
+		else {
+			System.out.println("Enter the value to be added to the linkedList : ");
+			int value = sc.nextInt();
+			Node node = new Node(value);
+			Node prev = head;
+			int count = 1;
+			while (count<pos-1) {
+				prev = prev.next;
+				count++;
+			}
+			Node current = prev.next;
+			prev.next = node;
+			node.next = current;
+			display();
+		}
+	}
+	
 	
 	public void display() {
 		Node curr = head;
@@ -45,9 +93,18 @@ public class Ssl {
 			curr = curr.next;
 		}
 		System.out.println("Null");
+		count();
 	}
 
-	
+	public void count() {
+		Node curr = head;
+		int counter = 0;
+		while (curr!=null) {
+			counter++;
+			curr = curr.next;
+		}
+		System.out.println("There are total "+counter+" elements in the LinkedList.");
+	}	
 	public static void main(String[] args) {
 		Ssl ll = new Ssl();
 		ll.head = new Node(10);
@@ -60,7 +117,7 @@ public class Ssl {
 		 while(true) {
 			 
 			 	System.out.println("Enter the suitable option : ");
-				System.out.println("1. Create\n2. Add element");
+				System.out.println("1. Create\n2. Add element in the end\n3. Add element in the start\n4. Add element in a certain position");
 				Scanner sc = new Scanner(System.in);
 				int opt = sc.nextInt();
 					
@@ -74,6 +131,19 @@ public class Ssl {
 				ll.add();
 				break;
 				
+			}
+			case 3: {
+				ll.addstart();
+				break;
+				
+			}
+			case 4: {
+				ll.addmiddle();
+				break;
+			}
+			case 5: {
+				ll.count();
+				break;
 			}
 			default:
 				throw new IllegalArgumentException("Unexpected value: " + opt);
