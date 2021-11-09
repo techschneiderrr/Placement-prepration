14b62323c7cd4eb53bd77ba294b5c530a1198665
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2021-11-09 12:44:25 +0530

    Update User.java

--- DSA/src/User.java
+++ DSA/src/User.java
@@ -1,4 +1,28 @@
+import java.util.Scanner;
 
 public class User {
-
+	
+	public static void main(String[] args) {
+		Scanner myObj = new Scanner(System.in);
+		int n = myObj.nextInt();
+		int net;
+		for (int i=0;i<n;i++)
+		{
+			int q = myObj.nextInt();
+			int p = myObj.nextInt();
+			if (q>1000)
+			{
+				net = p*q;
+				net = net-(net/10);
+				System.out.println(net);
+			}
+			else
+			{
+				net = p*q;
+				System.out.println(net);
+			}
+			
+		}
+		
+	}
 }
